bf61ba8e1d3e671ff5aaf37e31dee04d1d95982d
rmorriso <rmorriso@8fd88347-fc12-0410-a11b-a6b282cb9c29>
2006-05-31 20:50:51 +0000

    fix to DacsXmlBeansTest

--- trunk/nb-projects/dacs-xmlbeans/test/com/fedroot/dacs/xmlbeans/DacsXmlBeansTest.java
+++ trunk/nb-projects/dacs-xmlbeans/test/com/fedroot/dacs/xmlbeans/DacsXmlBeansTest.java
@@ -9,12 +9,14 @@ package com.fedroot.dacs.xmlbeans;
  */
 
 import junit.framework.*;
-import com.fedroot.dacs.xmlbeans.*;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.xmlbeans.XmlObject;
 
 
 /**
- * tests for dacs-services;
+ * tests for dacs-xmlbeans;
  * uses TestConstants
  */
 
@@ -25,169 +27,43 @@ public class DacsXmlBeansTest extends TestCase {
     // library which itself relies on this (dacs-services) library
     private String hmac;
     private String time;
+    private HttpClient httpclient;
+    private GetMethod httpget;
     
     /**
      * constructor for DacsXmlBeansTest
      */
     public DacsXmlBeansTest(String testName) {
         super(testName);
+        httpclient = new HttpClient(new MultiThreadedHttpConnectionManager());
+        httpclient.getHttpConnectionManager().getParams().setConnectionTimeout(15000);
+        httpget = new GetMethod("https://demo.fedroot.com/dacs/dacs_list_jurisdictions?FORMAT=XMLSCHEMA");
     }
     
-    public void testDacsAcsXml_902()
+    public void testListJurisdictionsXml()
     throws Exception {
-        System.out.println("testDacsAcsXml (902)");
+        System.out.println("testListJurisdictionsXml");
         try {
             // TODO - change getResponseBody() to read from file instead
-                XmlObject expectedXmlObject = XmlObject.Factory.parse(dacsget.getResponseBodyAsStream());
-                // Check that it is an instance of the DacsAcsDocument
-                System.out.println(expectedXmlObject.getClass().getName());
-                if(expectedXmlObject instanceof DacsAcsDocument) {
-                    DacsAcsDocument doc =
-                            (DacsAcsDocument)expectedXmlObject;
-                    assertTrue("invalid DacsAcsDocument", doc.validate());
-                    DacsAcsDocument.DacsAcs dacsacs = doc.getDacsAcs();
-                    assertTrue("wrong DACS return status", dacsacs.isSetAccessDenied());
-                    assertTrue("wrong access_denied event", dacsacs.getAccessDenied().isSetEvent902());
-                    
-                } else {
-                    fail("incorrect XML document type");
-                }
+            httpclient.executeMethod(httpget);
+            // DacsListJurisdictionsDocument expectedXmlObject = DacsListJurisdictionsDocument.Factory.parse(httpget.getResponseBodyAsStream());
+            XmlObject expectedXmlObject = XmlObject.Factory.parse(httpget.getResponseBodyAsStream());
+            // Check that it is an instance of the DacsAcsDocument
+            System.out.println(expectedXmlObject.getClass().getName());
+            System.out.println(expectedXmlObject.toString());
+            if(expectedXmlObject instanceof DacsListJurisdictionsDocument) {
+                DacsListJurisdictionsDocument doc =
+                        (DacsListJurisdictionsDocument)expectedXmlObject;
+                assertTrue("invalid DacsAcsDocument", doc.validate());
+                DacsListJurisdictionsDocument.DacsListJurisdictions jurs = doc.getDacsListJurisdictions();
+                assertEquals(9, jurs.getJurisdictionList().size());
+            } else {
+                fail("incorrect XML document type");
+            }
         } catch (Exception e) {
             fail("unexpected exception thrown: " + e.getMessage());
         }
     }
     
-    // this test must be run before testDacsConfXml which depends
-    // on authentication as an ADMIN_IDENTITY
-    public void testDacsAuthenticateXml()
-    throws Exception {
-        System.out.println("testDacsAuthenticateXml (failure mode 1)");
-        try {
-                XmlObject expectedXmlObject = XmlObject.Factory.parse(httpget.getResponseBodyAsStream());
-                // Check that it is an instance of the DacsListJurisdictionsDocument
-                System.out.println(expectedXmlObject.getClass().getName());
-                if(expectedXmlObject instanceof DacsAuthReplyDocument) {
-                    DacsAuthReplyDocument doc =
-                            (DacsAuthReplyDocument)expectedXmlObject;
-                    DacsAuthReplyDocument.DacsAuthReply authreply = doc.getDacsAuthReply();
-                    assertFalse(authreply.isSetDacsCurrentCredentials());
-                    assertTrue(authreply.isSetCommonStatus());
-                    assertTrue("invalid common_error element", authreply.getCommonStatus().validate());
-                    assertEquals(999, authreply.getCommonStatus().getCode());
-                    assertEquals("800 Authentication failed, invalid authenticating information (Authentication failed)", authreply.getCommonStatus().getMessage());
-                    
-                } else {
-                    fail("incorrect XML document type");
-                }
-        } catch (Exception e) {
-            fail("unexpected exception thrown");
-        }
-        System.out.println("testDacsAuthenticateXml (failure mode 2)");
-        try {
-                XmlObject expectedXmlObject = XmlObject.Factory.parse(httpget.getResponseBodyAsStream());
-                // Check that it is an instance of the DacsAuthReplyDocument
-                System.out.println(expectedXmlObject.getClass().getName());
-                if(expectedXmlObject instanceof DacsAuthReplyDocument) {
-                    DacsAuthReplyDocument doc =
-                            (DacsAuthReplyDocument)expectedXmlObject;
-                    DacsAuthReplyDocument.DacsAuthReply authreply = doc.getDacsAuthReply();
-                    assertFalse(authreply.isSetDacsCurrentCredentials());
-                    assertTrue(authreply.isSetCommonStatus());
-                    assertTrue("invalid CommonError", authreply.getCommonStatus().validate());
-                    assertEquals(999, authreply.getCommonStatus().getCode());
-                    assertEquals("800 Authentication failed, invalid authenticating information (Authentication failed)", authreply.getCommonStatus().getMessage());
-                    
-                } else {
-                    fail("incorrect XML document type");
-                }
-        } catch (Exception e) {
-            fail("unexpected exception thrown");
-        }
-        System.out.println("testDacsAuthenticateXml (success mode)");
-        try {
-                XmlObject expectedXmlObject = XmlObject.Factory.parse(httpget.getResponseBodyAsStream());
-                // Check that it is an instance of the DacsAuthReplyDocument
-                System.out.println(expectedXmlObject.getClass().getName());
-                if(expectedXmlObject instanceof DacsAuthReplyDocument) {
-                    DacsAuthReplyDocument doc =
-                            (DacsAuthReplyDocument)expectedXmlObject;
-                    DacsAuthReplyDocument.DacsAuthReply authreply = doc.getDacsAuthReply();
-                    System.out.println(authreply.toString());
-                    assertTrue(authreply.isSetDacsCurrentCredentials());
-                    assertFalse(authreply.isSetCommonStatus());
-                    assertTrue("invalid DacsCurrentCredentials", authreply.getDacsCurrentCredentials().validate());
-                    assertEquals("login failed", TestConstants.testadminusername, authreply.getDacsCurrentCredentials().getCredentialsArray(0).getName());
-                } else {
-                    fail("incorrect XML document type");
-                }
-        } catch (Exception e) {
-            fail("unexpected exception thrown");
-        }
-    }
     
-    public void testDacsAcsXml_905()
-    throws Exception {
-        System.out.println("testDacsAcsXml (905 secure)");
-        try {
-                XmlObject expectedXmlObject = XmlObject.Factory.parse(dacsget.getResponseBodyAsStream());
-                // Check that it is an instance of the DacsAcsDocument
-                System.out.println(expectedXmlObject.getClass().getName());
-                if(expectedXmlObject instanceof DacsAcsDocument) {
-                    DacsAcsDocument doc =
-                            (DacsAcsDocument)expectedXmlObject;
-                    System.out.println(doc.getDacsAcs().toString());
-                    assertTrue("invalid DacsAcsDocument", doc.validate());
-                    DacsAcsDocument.DacsAcs dacsacs = doc.getDacsAcs();
-                    assertTrue("wrong DACS return status", dacsacs.isSetAccessDenied());
-                    assertTrue("wrong access_denied event", dacsacs.getAccessDenied().isSetEvent905());
-                    assertTrue("hmac not set", dacsacs.getAccessDenied().getEvent905().isSetHmac());
-                    // set hmac and time for later use in dacs_notices ack test
-                    hmac = dacsacs.getAccessDenied().getEvent905().getHmac();
-                    time = dacsacs.getAccessDenied().getEvent905().getTime().toString();
-                } else {
-                    fail("incorrect XML document type");
-                }
-        } catch (Exception e) {
-            fail("unexpected exception thrown: " + e.getMessage());
-        }
-    }
-    
-    public void testDacsListJurisdictionsXml()
-    throws Exception {
-        System.out.println("testDacsListJurisdictionsXml");
-        try {
-                XmlObject expectedXmlObject = XmlObject.Factory.parse(httpget.getResponseBodyAsStream());
-                // Check that it is an instance of the DacsListJurisdictionsDocument
-                System.out.println(expectedXmlObject.getClass().getName());
-                if(expectedXmlObject instanceof DacsListJurisdictionsDocument) {
-                    DacsListJurisdictionsDocument doc =
-                            (DacsListJurisdictionsDocument)expectedXmlObject;
-                    assertTrue("invalid DacsListJurisdictionsDocument", doc.validate());
-                } else {
-                    fail("incorrect XML document type");
-                }
-        } catch (Exception e) {
-            fail("unexpected exception thrown");
-        }
-    }
-    
-    public void testDacsConfXml()
-    throws Exception {
-        System.out.println("testDacsConfXml");
-        try {
-                System.out.println(httpget.getResponseBodyAsString());
-                XmlObject expectedXmlObject = XmlObject.Factory.parse(httpget.getResponseBodyAsStream());
-                // Check that it is an instance of the DacsListJurisdictionsDocument
-                System.out.println(expectedXmlObject.getClass().getName());
-                if(expectedXmlObject instanceof DacsConfReplyDocument) {
-                    DacsConfReplyDocument doc =
-                            (DacsConfReplyDocument)expectedXmlObject;
-                    assertTrue("invalid DacsConfReplyDocument", doc.validate());
-                } else {
-                    fail("incorrect XML document type");
-                }
-        } catch (Exception e) {
-            fail("unexpected exception thrown");
-        }
-    }
 }
