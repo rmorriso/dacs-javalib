9c0568efd0406e4922bd2edae4d07305efc670ea
rmorriso <rmorriso@8fd88347-fc12-0410-a11b-a6b282cb9c29>
2010-02-24 01:15:44 +0000

    added DacsAuthenticate, DacsCurrentCredentials web service request classes

---
+++ trunk/dacs-core/src/main/java/fedroot/dacs/client/DacsCurrentCredentialsRequest.java
@@ -0,0 +1,30 @@
+/*
+ * DacsCurrentCredentialsRequest.java
+ *
+ * Created on February 23, 2010, 3:12 PM
+ *
+ * Copyright (c) 2010 Metalogic Software Corporation.
+ * All rights reserved. See http://fedroot.com/licenses/metalogic.txt for redistribution information.
+ */
+
+package fedroot.dacs.client;
+
+import fedroot.dacs.entities.Jurisdiction;
+import fedroot.dacs.DACS.ServiceName;
+import fedroot.web.ServiceParameters;
+import java.net.URISyntaxException;
+
+/**
+ *
+ * @author rmorriso
+ */
+public class DacsCurrentCredentialsRequest extends DacsWebServiceRequest {
+
+    public DacsCurrentCredentialsRequest(Jurisdiction jurisdiction) throws URISyntaxException {
+        super(jurisdiction.getDacsUri() + "/" + ServiceName.dacs_current_credentials);
+        ServiceParameters serviceParameters = new ServiceParameters();
+        serviceParameters.addParameter(null, null);
+    }
+
+
+}
