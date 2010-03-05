19ffc83f0cc0530c5db0bab5e81ef2d774cbd32b
rmorriso <rmorriso@8fd88347-fc12-0410-a11b-a6b282cb9c29>
2010-03-05 21:29:54 +0000

    added new constructor DacsCookie(domain, name, value)

--- dacs-core/src/main/java/fedroot/dacs/http/DacsCookie.java
+++ dacs-core/src/main/java/fedroot/dacs/http/DacsCookie.java
@@ -46,6 +46,26 @@ public class DacsCookie extends BasicClientCookie {
         setAttribute(ClientCookie.DOMAIN_ATTR, domain);
     }
 
+    public DacsCookie(String domain, String name, String value) throws DacsRuntimeException {
+        super(name, value);
+
+        if (!isDacsCookie(this)) {
+            throw new DacsRuntimeException("invalid DACS cookie: " + this.getName());
+        }
+
+        setVersion(1);
+        setDomain("." + domain);
+        setPath("/");
+
+        Date expires = new Date();
+        expires.setTime(expires.getTime() + 3600);
+
+        setSecure(false);
+        // Set attributes EXACTLY as sent by the server
+        setAttribute(ClientCookie.VERSION_ATTR, "1");
+        setAttribute(ClientCookie.DOMAIN_ATTR, domain);
+    }
+
     public static boolean isDacsCookie(org.apache.http.cookie.Cookie cookie) {
         return isDacsCookieName(cookie.getName());
     }
