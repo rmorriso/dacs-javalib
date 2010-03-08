49b292d552145d2318d1e2115b97684c260d5153
rmorriso <rmorriso@8fd88347-fc12-0410-a11b-a6b282cb9c29>
2010-03-08 17:29:11 +0000

    added DacsCookieName to encapsulate cookie name syntax

---
+++ dacs-core/src/main/java/fedroot/dacs/http/DacsCookieName.java
@@ -0,0 +1,106 @@
+/*
+ * DacsCookieName.java
+ * Created on Jan 15, 2010 8:24:49 PM.
+ * Copyright (c) 2010 Metalogic Software Corporation
+ * All rights reserved. See http://fedroot.com/licenses/metalogic.txt for redistribution information.
+ */
+package fedroot.dacs.http;
+
+import fedroot.dacs.exceptions.DacsRuntimeException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author ricmorri
+ */
+public class DacsCookieName {
+
+    private String cookieName;
+    private String federationPart;
+    private String jurisdictionPart;
+    private String usernamePart;
+    private boolean selectCookieName;
+
+    /** 
+     * a DACS cookie name is of the form DACS:Federation::Jurisdiction:Username
+     */
+    private DacsCookieName(String federationName, String jurisdictionName, String username) throws DacsRuntimeException {
+        federationPart = federationName;
+        jurisdictionPart = jurisdictionName;
+        usernamePart = username;
+        cookieName = "DACS:" + federationPart + "::" + jurisdictionPart + ":" + usernamePart;
+        selectCookieName = false;
+    }
+
+    /**
+     * a DACS cookie name is of the form DACS:Federation-Name:::SELECTED
+     */
+    private DacsCookieName(String federationName) throws DacsRuntimeException {
+        federationPart = federationName;
+        cookieName = "DACS:" + federationPart + ":::SELECTED";
+        selectCookieName = true;
+    }
+
+    /*
+     * TOTO parse cookieName into component parts
+     */
+    public static DacsCookieName valueOf(String cookieName) {
+        if (cookieName.startsWith("DACS:")) {
+            Pattern word = Pattern.compile("[\\w]+");
+            try {
+                String rest = cookieName.substring(cookieName.indexOf(":") + 1);
+                // look for DACS Select Credentials cookie name of the form
+                // DACS:Federation-Name:::SELECTED
+                if (rest.endsWith(":::SELECTED")) {
+                    String federationPart = rest.substring(0, rest.indexOf(":::SELECTED"));
+                    Matcher m = word.matcher(federationPart);
+                    if (m.matches()) {
+                        return new DacsCookieName(federationPart);
+                    }
+                } else {
+                // look for DACS Credentials cookie name of the form
+                // DACS:Federation-Name::Jurisdiction-Name:Username
+                    String federationPart = rest.substring(0, rest.indexOf("::"));
+                    Matcher m = word.matcher(federationPart);
+                    if (m.matches()) {
+                        rest = rest.substring(rest.indexOf("::") + 2);
+                        String jurisdictionPart = rest.substring(0, rest.indexOf(":"));
+                        m = word.matcher(jurisdictionPart);
+                        if (m.matches()) {
+                            rest = rest.substring(rest.indexOf(":") + 1);
+                            String usernamePart = rest;
+                            m = word.matcher(usernamePart);
+                            if (m.matches()) {
+                                return new DacsCookieName(federationPart, jurisdictionPart, usernamePart);
+                            }
+                        }
+                    }
+                }
+            } catch (IndexOutOfBoundsException ex) {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    public String getCookieName() {
+        return cookieName;
+    }
+    
+    public String getFederationPart() {
+        return federationPart;
+    }
+
+    public String getJurisdictionPart() {
+        return jurisdictionPart;
+    }
+
+    public String getUsernamePart() {
+        return usernamePart;
+    }
+
+    public boolean isSelectCookieName() {
+        return selectCookieName;
+    }
+}
