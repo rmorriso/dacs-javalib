ee4a421b4b61ed965486c1aa0b7a1b4996118294
rmorriso <rmorriso@8fd88347-fc12-0410-a11b-a6b282cb9c29>
2010-10-06 20:16:33 +0000

    initial import of old DACS Swing app demo

---
+++ demo/dacs-swing/src/main/java/com/fedroot/demo/dacs/swing/Main.java
@@ -0,0 +1,50 @@
+/*
+ * Example7_8_9_10.java
+ *
+ * Created on December 2, 2005, 8:28 PM
+ *
+ * Copyright (c) 2005 Metalogic Software Corporation.
+ * All rights reserved. See http://fedroot.com/licenses/metalogic.txt for redistribution information.
+ */
+
+package com.fedroot.demo.dacs.swing;
+
+import com.fedroot.dacs.Federation;
+import com.fedroot.dacs.UserContext;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * main class for examples 7, 8, 9, 10 in DJL system documentation;
+ * this is a simple Swing application to demonstrate various DACS operations
+ * @author Rick Morrison
+ */
+public class Main {
+    
+    private static UserContext usercontext = UserContext.getInstance("DacsClient Demo");
+
+    private static String feduri = "https://fedroot.com/fedadmin/dacs";
+    
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        try {
+            Federation federation = Federation.getInstance(usercontext, feduri);
+            DacsClientFrame f = new DacsClientFrame(usercontext, federation);
+            f.setTitle("DACS JavaLib Example Thick Client");
+            f.setSize(900, 500);
+            f.addWindowListener(
+                    new WindowAdapter() {
+                public void windowClosing(WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+            );
+            f.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
