a8cdefd4e66f1ed8322e5967631b9a1769039ec7
rmorriso <rmorriso@8fd88347-fc12-0410-a11b-a6b282cb9c29>
2010-12-06 07:19:31 +0000

    additions to dacs-core to allow conditional requests with event handling via check_fail

---
+++ dacs-core/src/main/java/fedroot/dacs/client/DacsCheckRequest.java
@@ -0,0 +1,63 @@
+/*
+ * DacsCheckRequest.java
+ *
+ * Created on February 23, 2010, 3:12 PM
+ *
+ * Copyright (c) 2010 Metalogic Software Corporation.
+ * All rights reserved. See http://fedroot.com/licenses/metalogic.txt for redistribution information.
+ */
+
+package fedroot.dacs.client;
+
+import fedroot.servlet.ParameterValidator;
+import fedroot.servlet.ParameterValidators;
+import fedroot.servlet.ServiceParameters;
+import fedroot.servlet.WebServiceRequest;
+
+
+/**
+ *
+ * @author rmorriso
+ */
+public class DacsCheckRequest extends WebServiceRequest {
+
+    public enum args { DACS_ACS };
+
+    private boolean checkOnly;
+
+    public DacsCheckRequest(String uri) {
+        this(uri,false);
+    }
+
+    public DacsCheckRequest(String uri, boolean checkOnly) {
+        super(uri);
+        this.checkOnly = checkOnly;
+    }
+
+
+    @Override
+    public ServiceParameters getServiceParameters() {
+       ServiceParameters serviceParameters = new ServiceParameters();
+       if (checkOnly) {
+           serviceParameters.addParameter(args.DACS_ACS, "-check_only%20-format+XMLSCHEMA");
+       } else {
+           serviceParameters.addParameter(args.DACS_ACS, "-check_fail%20-format+XMLSCHEMA");
+       }
+       return serviceParameters;
+    }
+
+    /**
+     * return ParameterValidators for use by clients that implement
+     * the DacsAuthenticateRequest service
+     * @return
+     */
+    @Override
+    public ParameterValidators getParameterValidators() {
+        ParameterValidators parameterValidators = new ParameterValidators();
+        ParameterValidator parameterValidator = new ParameterValidator(ParameterValidator.ValidationType.ALL);
+        parameterValidator.addParameter(args.DACS_ACS.toString());
+        parameterValidators.addValidator(parameterValidator);
+        return parameterValidators;
+    }
+
+}
