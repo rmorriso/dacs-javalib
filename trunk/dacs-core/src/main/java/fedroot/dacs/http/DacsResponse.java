9215589534271970d04b7ef097b82ccc1b184852
rmorriso <rmorriso@8fd88347-fc12-0410-a11b-a6b282cb9c29>
2011-01-06 05:12:32 +0000

    created DacsResponse object to encapsulate HttpResponse, HttpEntity etc

---
+++ trunk/dacs-core/src/main/java/fedroot/dacs/http/DacsResponse.java
@@ -0,0 +1,68 @@
+/*
+ * DacsResponse.java
+ * Created on Jan 05, 2011 8:02:49 PM.
+ * Copyright (c) 2011 Metalogic Software Corporation
+ * All rights reserved. See http://fedroot.com/licenses/metalogic.txt for redistribution information.
+ */
+package fedroot.dacs.http;
+
+import fedroot.dacs.exceptions.DacsException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.http.HttpResponse;
+import org.apache.http.entity.BufferedHttpEntity;
+
+/**
+ * a DACS wrapper class for HttpResponse
+ * @author rmorriso
+ */
+public class DacsResponse {
+
+    private HttpResponse httpResponse;
+    private InputStream inputStream;
+
+    private static final Logger logger = Logger.getLogger(DacsResponse.class.getName());
+
+    public DacsResponse(HttpResponse httpResponse) throws DacsException {
+        this.httpResponse = httpResponse;
+        if (httpResponse.getEntity() != null) {
+            try { // we need a BufferedHttpEntity so we can reset it after a DacsCheckRequest
+                this.inputStream = new BufferedHttpEntity(httpResponse.getEntity()).getContent();
+            } catch (IOException ex) {
+                logger.log(Level.SEVERE, null, ex);
+                throw new DacsException(ex.getLocalizedMessage());
+            }
+        }
+    }
+
+    /**
+     * @return the httpResponse
+     */
+    public HttpResponse getHttpResponse() {
+        return httpResponse;
+    }
+
+    /**
+     * @param httpResponse the httpResponse to set
+     */
+    public void setHttpResponse(HttpResponse httpResponse) {
+        this.httpResponse = httpResponse;
+    }
+
+    /**
+     * @return the inputStream
+     */
+    public InputStream getInputStream() throws DacsException {
+        return inputStream;
+    }
+
+    /**
+     * @param inputStream the inputStream to set
+     */
+    public void setInputStream(InputStream inputStream) {
+        this.inputStream = inputStream;
+    }
+
+}
