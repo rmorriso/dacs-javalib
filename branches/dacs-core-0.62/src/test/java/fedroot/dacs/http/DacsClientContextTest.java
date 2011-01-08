a736ad60ed22f953cdca8e70b110dab14f5c8912
rmorriso <rmorriso@8fd88347-fc12-0410-a11b-a6b282cb9c29>
2011-01-08 01:16:36 +0000

    versioned dacs-core 0.62

---
+++ branches/dacs-core-0.62/src/test/java/fedroot/dacs/http/DacsClientContextTest.java
@@ -0,0 +1,68 @@
+/*
+ * DacsClientContextTest.java
+ * Created on Jan 15, 2010 8:24:49 PM.
+ * Copyright (c) 2010 Metalogic Software Corporation
+ * All rights reserved. See http://fedroot.com/licenses/metalogic.txt for redistribution information.
+ */
+
+
+package fedroot.dacs.http;
+
+import java.net.URI;
+import java.util.Date;
+import java.util.List;
+import junit.framework.TestCase;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.utils.URIUtils;
+import org.apache.http.cookie.Cookie;
+
+/**
+ *
+ * @author Roderick Morrison <rmorriso at fedroot.com>
+ */
+public class DacsClientContextTest extends TestCase {
+
+    DacsClientContext dacsClientContext;
+
+    public DacsClientContextTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        dacsClientContext = new DacsClientContext();
+    }
+
+    public void testExecuteGetRequest() throws Exception {
+        URI uri = URIUtils.createURI("https", "fedroot.com", -1, "/dacs/dacs_version", "FORMAT=XML", null);
+        DacsGetRequest dacsGetRequest = new DacsGetRequest(uri);
+        DacsResponse dacsResponse = dacsClientContext.executeGetRequest(dacsGetRequest);
+        HttpEntity httpEntity = dacsResponse.getHttpResponse().getEntity();
+        assertNotNull(httpEntity);
+        assertEquals(200, dacsResponse.getHttpResponse().getStatusLine().getStatusCode());
+        assertEquals("Content-Type: text/xml", httpEntity.getContentType().toString());
+    }
+
+    public void testGetAllCookies() throws Exception {
+        URI uri = URIUtils.createURI("http", "www.google.com", -1, "/search", "q=httpclient&btnG=Google+Search&aq=f&oq=", null);
+        DacsGetRequest dacsGetRequest = new DacsGetRequest(uri);
+        DacsResponse dacsResponse = dacsClientContext.executeGetRequest(dacsGetRequest);
+        List<Cookie> cookies = dacsClientContext.getAllCookies();
+        assertEquals(2, cookies.size());
+        for (Cookie cookie : cookies) {
+            System.out.println(cookie.getName());
+            assertFalse(cookie.isSecure());
+            assertFalse(cookie.isExpired(new Date()));
+            assertFalse(DacsCookie.isDacsCookie(cookie));
+        }
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        dacsClientContext.closeConnection();
+    }
+
+}
